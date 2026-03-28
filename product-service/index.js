b6261735fa587503c24b22efe8819b1ad56e24bb
ducvd <vuduyduc550@gmail.com>
2026-03-29 00:33:51 +0700

    .

--- product-service/index.js
+++ product-service/index.js
@@ -124,6 +124,6 @@ app.use(express.urlencoded({ extended: true }));
 // });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0',() => {
     console.log(`Server running on port ${PORT}`);
 });
